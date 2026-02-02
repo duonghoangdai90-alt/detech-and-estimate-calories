525af84d74162352f55f453f6d3ee03da586f205
Hoang Dai Duong <duonghoangdai90@gmail.com>
2026-02-03 00:46:40 +0700

    Refactor main function for food input and display
    Refactor main function to improve food input handling and display.

--- A.project/main.cpp
+++ A.project/main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include<limits>
 #include "name_food.h"
 #include "quantity_food.h"
 
 int main(){
-    name_food nf;
-    quantity_food qf(0);
-    std::vector<std::string> v(3);
-    
-    qf.input();
-    std::cout<<"so mon co trong anh la : "<<qf.display();
-    qf.display();
+    Name_food nf;
+    quantity_food qf;
+    int n;
+    std::cout<<"nhap so mon = ";
+    std::cin>>n;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    qf.quantity(n);
+    std::cout<<"so mon = "<<qf.display()<<std::endl;
 
-    nf.input_nf(qf, v);
+    std::vector<std::string> v;
+    v.resize(qf.getquantity());
+    for (int i = 0; i < qf.getquantity(); i++){
+        getline(std::cin,v[i]);
+    }
+    nf.setFood(v);
+
+    std::cout<<"ds mon : "<<std::endl;
     nf.display();
 
     return 0;
