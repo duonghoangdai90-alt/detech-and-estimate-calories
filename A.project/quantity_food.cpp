296da31cc13512b781258ef3ca57682e30cd24de
Hoang Dai Duong <duonghoangdai90@gmail.com>
2026-02-03 00:48:30 +0700

    Refactor quantity_food class implementation
    Refactor quantity_food class to use quantity_sl and add setter/getter methods.

--- A.project/quantity_food.cpp
+++ A.project/quantity_food.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 #include"quantity_food.h"
 
-quantity_food::quantity_food(int quantity):quantity(quantity){};
+quantity_food::quantity_food(const int initiaint):quantity_sl(initiaint){}
 
-void quantity_food::input(){
-    std::cout<<"nhap so mon: ";
-    std::cin>>quantity;
+void quantity_food::quantity(int newquan){
+    this->quantity_sl = newquan;
+}   
+const int quantity_food::getquantity() const{
+    return quantity_sl;
 }
+
 int quantity_food::display() const{
-    return quantity;
+    return quantity_sl;
 }
